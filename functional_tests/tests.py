0c921dcab4a4954ad6f0962f7b9045a79d43f7e9
daonan <91860300+daonan233@users.noreply.github.com>
2024-05-28 22:49:14 +0800

    10 finish

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,26 +1,39 @@
 from selenium import webdriver
+from selenium.common import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 from selenium.webdriver.common.by import By
+from django.test import LiveServerTestCase
+from webdriver_manager.chrome import ChromeDriverManager
+MAX_WAIT = 10
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID,'id_list_table')
+                rows = table.find_elements(By.TAG_NAME,'tr')
+                self.assertIn(row_text,[row.text for row in rows])
+                return
+            except(AssertionError,WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 张三听说有一个在线待办事项的应用
         # 他去看了这个应用的首页
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 他注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
@@ -40,9 +53,8 @@ class NewVisitorTest(unittest.TestCase):
 
         # 待办事项表格中显示了“1: Buy flowers”
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('1: Buy flowers')
 
         # 页面中又显示了一个文本输入框，可以输入其他待办事项
 
@@ -51,13 +63,12 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Give a gift to Lisi')
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
 
-        self.check_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('1: Buy flowers')
 
-        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        self.wait_for_row_in_list_table('2: Give a gift to Lisi')
 
         # 张三想知道这个网站是否会记住她的清单
 
