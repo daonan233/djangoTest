4d6530c0c798c51590adc2f781c8f3e23d4ceef2
daonan <91860300+daonan233@users.noreply.github.com>
2024-05-28 13:24:35 +0800

    9 finish

--- lists/tests.py
+++ lists/tests.py
@@ -1,14 +1,22 @@
 from django.http import HttpRequest
 from django.urls import resolve
 from django.test import TestCase
+from selenium.webdriver.common.by import By
+
 from lists.views import home_page
+from selenium import webdriver
 
 
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response,'home.html')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+
 
     # def test_home_page_return_correct_html(self):
     #     request = HttpRequest()
